00cba0f5f1d4fa78e1e75fe2c69356862788529d
Abdallah Hossam <53866723+Abdallah504@users.noreply.github.com>
2025-04-28 16:06:46 +0300

    main file adding screenutil package and easy localization package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopping/view-model/core/di.dart';
 import 'package:shopping/view/screens/splash-screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await init();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations', // Path to translation files
+      fallbackLocale:  Locale('en'), // Default locale
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,15 +24,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shopping',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,_){
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Shopping',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          locale: context.locale, // Locale from EasyLocalization
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+          home: SplashScreen(),
+        );
+      },
     );
   }
 }
