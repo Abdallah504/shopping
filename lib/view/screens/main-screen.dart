a88ade32d91dbc83ea48e093aecda2b7df9a11d9
Abdallah Hossam <53866723+Abdallah504@users.noreply.github.com>
2025-04-28 15:59:41 +0300

    initial screens

--- lib/view/screens/main-screen.dart
+++ lib/view/screens/main-screen.dart
@@ -1,4 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping/view-model/core/constant.dart';
+import 'package:shopping/view/screens/cart-screen.dart';
+import 'package:shopping/view/screens/cat-screen.dart';
+import 'package:shopping/view/screens/home-screen.dart';
+import 'package:shopping/view/screens/profile-screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,12 +14,51 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    CategoryScreen(),
+    CartScreen(),
+    ProfileScreen()
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text('Hello' , style: TextStyle(color: Colors.black),),
+      backgroundColor: AppColors().white,
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors().orange,
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items:  [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home'.tr(),
+            backgroundColor: AppColors().orange
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'categories'.tr(),
+              backgroundColor: AppColors().orange
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket),
+            label: 'cart'.tr(),
+              backgroundColor: AppColors().orange
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'profile'.tr(),
+              backgroundColor: AppColors().orange
+          ),
+        ],
       ),
     );
   }
